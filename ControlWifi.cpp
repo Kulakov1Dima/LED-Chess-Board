a52bf2ae665a57fd7f7ae7270e47c4210f7b0397
Tesupersnake <supersnakeplay@gmail.com>
2023-01-05 01:04:31 +0300

    e

--- ControlWifi.cpp
+++ ControlWifi.cpp
@@ -1,1 +1,17 @@
 #include <ESP8266WiFi.h>
+
+#include "ControlWifi.h"
+
+CWIFI::CWIFI(char* network_name, char* network_password) {                //конструктор класса
+  _network_name = network_name;                                           //имя сети(динамическое)
+  _network_password = network_password;                                   //пароль сети
+}
+
+bool CWIFI::connectWiFi(int timeout, uint8_t led) {
+  WiFi.begin(_network_name, _network_password);
+  analogWrite(led, 0);
+  // Функция подключения к wifi, пытается подключиться к сети timeout времени, при неуспешном подключении возвращает false иначе true
+  //во время подключения мигает светодиодом
+  //если функция вернула true светодиод останется включенным
+  return true;
+}
