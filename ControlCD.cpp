7fc2cf501b39e158fab28560bc4fdac3a893abb7
Dmitry <116003850+DmitriiFedor@users.noreply.github.com>
2023-01-06 17:05:56 +0300

    Add files via upload

--- ControlCD.cpp
+++ ControlCD.cpp
@@ -1,8 +1,14 @@
 #include <ESP8266WiFi.h>
 #include "ControlCD.h"
+#include <SPI.h>
+#include <SD.h>
+#include "Configuration.h"
 
 
 bool CCD::initializingCDcard(uint8_t pin) {
   //функция проверки cd карты на её наличие
-  return false;
+   if (SD.begin(CDPIN)) 
+    
+  
+  return true;
 }
